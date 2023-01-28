22296053338e6aaa941f5fdf40a6ba6f33e0d9c8
Sergey Melekhin <sergey@melekhin.me>
2023-01-28 16:09:31 +0700

    all signature examples from docs as tests

--- pkg/connectors/binance/auth_test.go
+++ pkg/connectors/binance/auth_test.go
@@ -24,6 +24,22 @@ func TestSign(t *testing.T) {
 			"1499827319559",
 			"c8db56825ae71d6d79447849e617115f4a920fa2acdcab2b053c4b2838bd6b71",
 		},
+		{
+			"Example 2: As a query string",
+			"NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j",
+			"symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000",
+			"",
+			"1499827319559",
+			"c8db56825ae71d6d79447849e617115f4a920fa2acdcab2b053c4b2838bd6b71",
+		},
+		{
+			"Example 3: Mixed query string and request body",
+			"NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j",
+			"symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC",
+			"quantity=1&price=0.1&recvWindow=5000",
+			"1499827319559",
+			"0fd168b8ddb4876a0358a8d14d0c9f3da0e9b20c5d52b2a00fcf7d1c602f9a77",
+		},
 	}
 
 	for _, c := range cases {
